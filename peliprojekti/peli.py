78eb6c18d1f03f2faa68d9fdd8690dbba3e9c37f
Mihail Nedashkovsky <mihaa.ned@gmail.com>
2026-09-24 19:43:50 +0300

    projekti 2

--- peliprojekti/peli.py
+++ peliprojekti/peli.py
@@ -1,4 +1,31 @@
 name = input("Mikä on nimesi:")
-age  = input("Kuinka vanha olet: ")
+age  = int(input("Kuinka vanha olet: "))
+if age <= 12:
+    print("Olet alaikäinen, niin et voi pelata peliä.")
+else:
+    print(f"Tervetuloa peliin, {name}!")
+
+while True:
+        print("....GAME MENU....")
+        print("1. aloita peli")
+        print("2. ohjeet")
+        print("3. sulje peli")
+
+        action = int(input("Mitä haluat tehdä? "))
+
+        if action == 1:
+            print("Peli alkaoi. Gl!")
+
+        elif action == 2:
+            print("ohjeita ei vielä ole.")
+
+        elif action == 3:
+            print("Peli suljetaan.")
+            break
+
+        else:
+            print("Komentoa ei löydy.")
+            break
+
 print(name)
 print(age)
